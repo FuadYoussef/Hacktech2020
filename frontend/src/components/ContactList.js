9bcb2de66a0cda333c041e585eb0581eb44e4e10
Colin Peppler <colinpeppler@gmail.com>
2020-03-07 14:25:12 -0800

    The beginning of the event card implementation:

--- frontend/src/components/ContactList.js
+++ frontend/src/components/ContactList.js
@@ -6,7 +6,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import Card from "@material-ui/core/Card";
 import Paper from "@material-ui/core/Paper";
 
 
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,10 +1,12 @@
 import ContactList from "./ContactList";
+import EventList from "./EventList";
 import React from "react";
 
 export default function Sidebar() {
   return(
     <div>
       <ContactList/>
+      <EventList/>
     </div>
   );
 }
