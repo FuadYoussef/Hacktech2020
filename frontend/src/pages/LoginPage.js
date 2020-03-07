cb8b7cf34e6131c038c68ce52aea6a9873cc7251
Colin Peppler <colinpeppler@gmail.com>
2020-03-07 11:52:48 -0800

    Merge firebase authentication with new style changes

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -5,6 +5,8 @@ import { withRouter, Redirect } from "react-router";
 import app from "../base.js";
 import { AuthContext } from "../Auth.js";
 import SuccessPage from './SuccessPage';
+import {Link} from 'react-router-dom'
+import Button from "@material-ui/core/Button";
 
 /* Wrapper is used to position LoginComponent right in the center
 *  NOTE: min-height: 100vh ensure the div is the same height as the screen
@@ -25,8 +27,7 @@ const LoginComponent = styled.form`
   flex-direction: column;
   justify-content: space-between;
   align-items: center; 
-  height: 25vh;
-  width: 100%;
+  height: 30vh;
 `;
 
 const LoginButton = styled.button`
@@ -60,62 +61,30 @@ const Login = ({ history }) => {
 
   const { currentUser } = useContext(AuthContext);
 
-  if (currentUser) {
-    return <Redirect to="/SuccessPage" />;
-  }
+  /* Disabled for now */
+  // if (currentUser) {
+  //   return <Redirect to="/SuccessPage" />;
+  // }
 
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-    </div>
+    <Wrapper>
+      <h1 style={{paddingBottom: '8px'}}> Login </h1>
+      <LoginComponent onSubmit={handleLogin}>
+        <TextField id="outlined-basic" label="email" variant="outlined"/>
+        <TextField id="outlined-basic" label="password" variant="outlined"/>
+
+        <Link to="/" underline="none">
+          <Button variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+        </Link>
+        <Link style={{fontSize: '12px'}} to="/"> Forgot your password? </Link>
+
+        <div class="horizontal divider">
+          </div>
+      </LoginComponent>
+    </Wrapper>
   );
 };
 
 export default withRouter(Login);
-
-/*
->>>>>>> 34e498cdb61cbd57cc43f0b7bc327952727e2215
-export default class LoginPage extends Component {
-    render() {
-        return(
-          <Wrapper>
-<<<<<<< HEAD
-            <LoginComponent>
-              <TextField id="outlined-basic" label="email" variant="outlined"/>
-              <TextField id="outlined-basic" label="password" variant="outlined"/>
-
-              <Link to="/" underline="none">
-                <LoginButton>
-                  Login
-                </LoginButton>
-              </Link>
-              <a href="./">Forgot your password?</a>
-
-              <div class="horizontal divider">
-                {/* OR: google login/ facebook login */}
-//               </div>
-//             </LoginComponent>
-// =======
-//             <TextField id="outlined-basic" label="email" variant="outlined"/>
-//             <TextField id="outlined-basic" label="password" variant="outlined"/>
-//             <Button as="a" href="/">Login</Button>
-//             <a href="./">Forgot your password?</a>
-//             <div class="horizontal divider">
-//               //{OR: google login/ facebook login }
-//             </div>
-// >>>>>>> 34e498cdb61cbd57cc43f0b7bc327952727e2215
-//           </Wrapper>
-//         )
-//     }
-// }*/
