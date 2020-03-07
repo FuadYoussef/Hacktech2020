f0f9628a2022e885626b0e3410e523163bf76192
Colin Peppler <colinpeppler@gmail.com>
2020-03-07 03:22:02 -0800

    Create create profile & login buttons, also added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from '@material-ui/core/Button';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
+
   );
 }
+
+const Home = () => (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Hello teammates
+            </p>
+            <div>
+                <Button variant="contained" color="primary">
+                    Create a profile
+                </Button>
+
+                <Link to="/login" underline="none">
+                    <Button variant="contained" color="primary">
+                        Login
+                    </Button>
+                </Link>
+            </div>
+        </header>
+    </div>
+);
+
+const Login = () => (
+    <div>
+        <h1>Login</h1>
+    </div>
+);
 
 export default App;
