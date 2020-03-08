ced2221b66e83d06adfb43469808373c286c28ef
Colin Peppler <colinpeppler@gmail.com>
2020-03-07 17:04:47 -0800

    Chaned up dashboardpage

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
-import Sidebar from "../components/Sidebar";
+import styled from "styled-components";
+import SideBar from "../components/SideBar";
+import MenuBar from "../components/MenuBar"
+
+const DashboardContainer = styled.div`
+  display: flex;
+`
+
+const RightSide = styled.div`
+  flex-grow: 1;
+  background: green;
+  max-height: 100vh;
+`
 
 export default class DashboardPage extends Component {
   render() {
     return(
-      <div>
-        <Sidebar/>
-      </div>
+      <DashboardContainer>
+        <SideBar/>
+        <RightSide>
+          <MenuBar/>
+        </RightSide>
+      </DashboardContainer>
     );
   }
 }
