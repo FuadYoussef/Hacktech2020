35f20d03579966cd441f4465609c29fd95bc0e1c
Fuad Youssef <fuadmyoussef@gmail.com>
2020-03-08 03:24:12 -0400

    started adding messages

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Form from './Form.js';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import app from "../base.js";
 
-export default function ChatPage() {
-  return(
-    <div>
-      <h1>HI</h1>
-    </div>
-  );
+class ChatPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    }
+  }
+  componentDidMount() {
+    app.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        <div className="app__list">
+          <Form user={this.state.user} />
+        </div>
+      </div>
+    );
+  }
 }
+export default ChatPage;
---
+++ frontend/src/pages/Form.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './Form.css';
+import Message from './Message';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import app from "../base.js";
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Sebastian',
+      message: '',
+      list: [],
+    };
+    this.messageRef = app.database().ref().child('messages');
+    this.listenMessages();
+  }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.user) {
+      this.setState({'userName': nextProps.user.displayName});
+    }
+  }
+  handleChange(event) {
+    this.setState({message: event.target.value});
+  }
+  handleSend() {
+    if (this.state.message) {
+      var newItem = {
+        userName: this.state.userName,
+        message: this.state.message,
+      }
+      this.messageRef.push(newItem);
+      this.setState({ message: '' });
+    }
+  }
+  handleKeyPress(event) {
+    if (event.key !== 'Enter') return;
+    this.handleSend();
+  }
+  listenMessages() {
+    this.messageRef
+      .limitToLast(10)
+      .on('value', message => {
+        this.setState({
+          list: Object.values(message.val()),
+        });
+      });
+  }
+  render() {
+    return (
+      <div className="form">
+        <div className="form__message">
+          { this.state.list.map((item, index) =>
+            <Message key={index} message={item} />
+          )}
+        </div>
+        <div className="form__row">
+          <input
+            className="form__input"
+            type="text"
+            placeholder="Type message"
+            value={this.state.message}
+            onChange={this.handleChange.bind(this)}
+            onKeyPress={this.handleKeyPress.bind(this)}
+          />
+          <button
+            className="form__button"
+            onClick={this.handleSend.bind(this)}
+          >
+            send
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
