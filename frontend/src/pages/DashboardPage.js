8b2fd3d95cd8720ab1c332eacb5615a919d9fe2c
Colin Peppler <colinpeppler@gmail.com>
2020-03-07 17:02:32 -0800

    Fixed upper case

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import Sidebar from "../components/Sidebar";
+import SideBar from '../components/SideBar'
 
 export default class DashboardPage extends Component {
   render() {
     return(
       <div>
-        <Sidebar/>
+        <SideBar/>
       </div>
     );
   }
