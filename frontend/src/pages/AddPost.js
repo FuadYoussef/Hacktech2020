10601a8329b72a4423ff6889471270861a3cb73a
Liying Liang <liyingliang@Liyings-MacBook-Pro.local>
2020-03-08 01:03:34 -0800

    add buttons

--- frontend/src/pages/AddPost.js
+++ frontend/src/pages/AddPost.js
@@ -2,6 +2,19 @@ import React from 'react';
 import styled from "styled-components";
 import TextField from '@material-ui/core/TextField'
 import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+
+
+const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+  }));
 
 const BaseContainer = styled.form`
   font-size: 1.5em; 
@@ -23,6 +36,7 @@ const Wrapper = styled.div`
 `;
 
 export default function AddPost() {
+    const classes = useStyles();
     return (
         <Wrapper>
             <BaseContainer>
@@ -33,7 +47,35 @@ export default function AddPost() {
                         rows="10" cols="60">
                 </textarea>
 
-                <Button variant="contained" color="primary" type="submit">Speak Up</Button>
+                {/* <Button variant="contained" color="primary" type="submit">Speak Up</Button> */}
+                <div>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                        startIcon={<CloudUploadIcon />}
+                    >
+                        Upload
+                    </Button>
+
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                        startIcon={<DeleteIcon />}
+                    >
+                        Delete
+                    </Button>
+                
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                        startIcon={<SaveIcon />}
+                    >
+                        Save
+                    </Button>
+                    </div>
             </BaseContainer>
         </Wrapper>
     )
