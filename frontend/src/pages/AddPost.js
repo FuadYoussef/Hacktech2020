c26dc67d903101e1c020a393d9a97cba165f956c
Liying Liang <liyingliang@Liyings-MacBook-Pro.local>
2020-03-08 00:50:10 -0800

    add post page

--- frontend/src/pages/AddPost.js
+++ frontend/src/pages/AddPost.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import styled from "styled-components";
+import TextField from '@material-ui/core/TextField'
+import Button from "@material-ui/core/Button";
+
+const BaseContainer = styled.form`
+  font-size: 1.5em; 
+  padding: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center; 
+  height: 60vh;
+  font: 'Muli', sans-serif;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
 
 export default function AddPost() {
     return (
-        <div className="col-md-5">
-          <div className="form-area">  
-                <form role="form">
-                <br styles="clear:both" />
-                <div className="form-group">
-                    <input type="text" className="form-control" id="title" name="title" placeholder="Title" required />
-                </div>
-                
-                <div className="form-group">
-                <textarea className="form-control" type="textarea" id="subject" placeholder="Subject" maxlength="140" rows="7"></textarea>
-                </div>
-                   
-                <button type="button" id="submit" name="submit" className="btn btn-primary pull-right">Add Post</button>
-              </form>
-            </div>
-        </div>
+        <Wrapper>
+            <BaseContainer>
+                <TextField name="title" label="title" id="outlined-basic" variant="outlined"/>
+                <TextField name="subjct" label="subject" id="outlined-basic" variant="outlined"/>
+                <label for="story">Tell us your story:</label>
+                <textarea id="story" name="story"
+                        rows="10" cols="60">
+                </textarea>
+
+                <Button variant="contained" color="primary" type="submit">Speak Up</Button>
+            </BaseContainer>
+        </Wrapper>
     )
 }
